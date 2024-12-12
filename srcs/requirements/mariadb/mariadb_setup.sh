eef1d2da935087ef5248892c72b12707af8cfef7
Vincent Stineau <vstineau@MADE-f0Cr3s4.clusters.42paris.fr>
2024-12-12 15:28:36 +0100

    script bash for init mariadb miss check if

--- srcs/requirements/mariadb/mariadb_setup.sh
+++ srcs/requirements/mariadb/mariadb_setup.sh
@@ -1,2 +1,17 @@
-echo mariadb is runing
+if 
+
+mariadb-install-db --user=mysql
+
+service mariadb start
+
+mysql -e create database ${MYSQL_DATABASE};
+
+mysql -e create user '${MYSQL_USER}'@'localhost' identified by '${MYSQL_PASSWD}';
+
+mysql -e grant all privileges on DATABASE_NAME.* TO '${MYSQL_USER}'@'localhost';
+
+mysql -e flush privileges;
+
+fi
+
 exec "$@"
