577890bf915111d3e057fc99c6388d3bacce71f8
Vincent Stineau <vstineau@MADE-f0Cr4s6.clusters.42paris.fr>
2024-12-13 15:59:12 +0100

    script bash for init mariadb

--- srcs/requirements/mariadb/mariadb_setup.sh
+++ srcs/requirements/mariadb/mariadb_setup.sh
@@ -1,17 +1,19 @@
-if 
+
+if [[ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]]; then ;
 
 mariadb-install-db --user=mysql
 
 service mariadb start
 
-mysql -e create database ${MYSQL_DATABASE};
+mysql -e "create database \`${MYSQL_DATABASE}\`;"
 
-mysql -e create user '${MYSQL_USER}'@'localhost' identified by '${MYSQL_PASSWD}';
+mysql -e "create user '${MYSQL_USER}'@'localhost' identified by '${MYSQL_PASSWD}';"
 
-mysql -e grant all privileges on DATABASE_NAME.* TO '${MYSQL_USER}'@'localhost';
+mysql -e "grant all privileges on DATABASE_NAME.* TO '${MYSQL_USER}'@'localhost';"
 
-mysql -e flush privileges;
+mysql -e "flush privileges;"
 
 fi
 
 exec "$@"
+
