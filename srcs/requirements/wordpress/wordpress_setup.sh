4895afab35892fd3ca9cac166497f2ded59ee362
Vincent Stineau <vstineau@bess-f2r4s16.clusters.42paris.fr>
2024-12-16 17:51:52 +0100

    debut script wp cli

--- srcs/requirements/wordpress/wordpress_setup.sh
+++ srcs/requirements/wordpress/wordpress_setup.sh
@@ -1,1 +1,7 @@
+wp core download
+
+wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_USER --dbhost=localhost
+
+wp core install --url=$DOMAIN_NAME --title=$SITE_NAME --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PASSWD --admin_email=$ADMIN_EMAIL
+
 
