240e1b72871e696f34c52a74d6feda9344799924
Vincent Stineau <vstineau@bess-f2r2s17.clusters.42paris.fr>
2024-12-18 15:35:58 +0100

    wordpress cli script + no alpine debian only

--- srcs/requirements/wordpress/wordpress_setup.sh
+++ srcs/requirements/wordpress/wordpress_setup.sh
@@ -1,7 +1,16 @@
-wp core download
+#set -e #premier commande qui fail quite le scritp
 
-wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_USER --dbhost=localhost
+if [ ! -d "/var/www/" ]; then 
 
-wp core install --url=$DOMAIN_NAME --title=$SITE_NAME --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PASSWD --admin_email=$ADMIN_EMAIL
+wp core --allow-root download
 
+wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWD --dbhost=mariadb:3306 
+
+wp core install --allow-root --url=$DOMAIN_NAME --title=$SITE_NAME --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PASSWD --admin_email=$ADMIN_EMAIL
+
+wp user create --allow-root $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWD
+
+fi
+
+exec "$@"
 
