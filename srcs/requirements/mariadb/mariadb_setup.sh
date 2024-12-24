07d236ae95b0698533ce1cb1894c84efb1f1fa61
vstineau <stineauv@gmail.com>
2024-12-24 13:30:21 +0100

    do not name database database dummy

--- srcs/requirements/mariadb/mariadb_setup.sh
+++ srcs/requirements/mariadb/mariadb_setup.sh
@@ -11,7 +11,8 @@ if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
 				mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
 				
 				mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWD}';"
-				mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%'localhost';"
+
+				mysql -e "GRANT ALL on ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'localhost';"
 				
 				mysql -e "FLUSH PRIVILEGES;"
 
