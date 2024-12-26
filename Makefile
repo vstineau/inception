a541a1b43450f596512b3a68c4b2f6e98b26d654
vstineau <vstineau@student.42.fr>
2024-12-26 15:49:19 +0100

    -d && --build add in makefile

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 all:
 	mkdir --parents ~/data/mariadb ~/data/wordpress
-	docker compose -f srcs/docker-compose.yml up 
+	docker-compose -f srcs/docker-compose.yml up -d --build 
 clean:
-	docker compose -f srcs/docker-compose.yml down 
+	docker-compose -f srcs/docker-compose.yml down 
 	rm -rf ~/data/mariadb ~/data/wordpress
 
 prune:
--- srcs/requirements/wordpress/wordpress_setup.sh
+++ srcs/requirements/wordpress/wordpress_setup.sh
@@ -4,8 +4,6 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 
 wp-cli.phar core --allow-root download --path="/var/www/html"
 
-#rm /var/www/html/wp-config-sample.php
-
 wp-cli.phar config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWD --url=$DOMAIN_NAME --dbhost="mariadb" --skip-check --path="/var/www/html"
 
 wp-cli.phar core install --allow-root --url=$DOMAIN_NAME --title=$SITE_NAME --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PASSWD --admin_email=$ADMIN_EMAIL --path="/var/www/html"
